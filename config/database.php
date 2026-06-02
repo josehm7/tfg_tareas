a7b64a94bde0d396e9e08eb9869eb6a7c8d6438c
Jose Hernández Molina <josehernandezmolina700@gmail.com>
2026-06-02 18:25:53 +0200

    Añadido Composer, variables de entorno y librerías PHPMailer, Dotenv, TCPDF

--- config/database.php
+++ config/database.php
@@ -1,8 +1,15 @@
 <?php
-$host = 'localhost';
-$dbname = 'gestion_tareas';
-$username = 'root';
-$password = '';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'] ?? 'localhost';
+$dbname = $_ENV['DB_NAME'] ?? 'gestion_tareas';
+$username = $_ENV['DB_USER'] ?? 'root';
+$password = $_ENV['DB_PASS'] ?? '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
